6c0c432d14a2d0c28afbb69913c803483c7cdcc0
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-30 20:38:12 +0600

    Update Shourov.js

--- Shourov.js
+++ Shourov.js
@@ -2,7 +2,7 @@ const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
 function startProject() {
-	const child = spawn("node", ["Shourov.js"], {
+	const child = spawn("node", ["ShourovSm.js"], {
 		cwd: __dirname,
 		stdio: "inherit",
 		shell: true
