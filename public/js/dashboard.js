c58c82cf630a42bf76ef22ffa34cc6b4df69fd6d
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-31 18:37:55 +0600

    Create js/dashboard.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,21 @@
+// 1️⃣ সব command আনতেছি
+fetch("/api/commands")
+.then(res => res.json())
+.then(commands => {
+
+  const box = document.getElementById("cmds");
+
+  // 2️⃣ এক এক করে checkbox বানাচ্ছি
+  commands.forEach(cmd => {
+    box.innerHTML += `
+      <label>
+        <input type="checkbox"
+          ${cmd.enabled ? "checked" : ""}
+          onchange="toggleCommand('${cmd.name}', this.checked)">
+        ${cmd.name}
+      </label>
+      <br>
+    `;
+  });
+
+});
