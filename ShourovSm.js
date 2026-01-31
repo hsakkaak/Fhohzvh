a7318291b2695ef82d218ba33c3ce22290fb29e4
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-31 16:06:06 +0600

    Update ShourovSm.js

--- ShourovSm.js
+++ ShourovSm.js
@@ -177,11 +177,11 @@ global.temp = {
 // ———————————————— DASHBOARD ROUTES ———————————————— //
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname,'dashboard', 'dist','public','index.html'));
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/appstate', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dashboard','dist','public','index.html'));
+	res.sendFile(path.join(__dirname, 'public/appstate.html'));
 });
 
 app.get("/api/stats", (req, res) => {
