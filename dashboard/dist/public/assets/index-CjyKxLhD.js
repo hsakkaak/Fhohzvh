cb9fe70373ec54553ac01fa0696b6716dcc04b4f
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-01-31 01:59:09 -0800

    Delete dashboard/dist/public directory

--- dashboard/dist/public/assets/index-CjyKxLhD.js
+++
@@ -1,8 +0,0 @@
-function FS(t,e){for(var n=0;n<e.length;n++){const i=e[n];>
-`+A+r}var ve=!1;function we(r,s){if(!r||ve)return"";ve=!0;>
-`),g=u.stack.split(`
-`),v=f.length-1,P=g.length-1;1<=v&&0<=P&&f[v]!==g[P];)P--;>
-`+f[v].replace(" at new "," at ");return r.displayName&&_.>
-`).replace(Y1,"")}function Pa(r,s,a){if(s=qp(s),qp(r)!==s&>
-Error generating stack: `+g.message+`
-`+g.stack}return{value:r,source:s,stack:f,digest:null}}fun>
