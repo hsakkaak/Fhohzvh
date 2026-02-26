469d84364d838210deeb43833f83e2b73f6ea646
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-02-26 00:58:25 -0800

    Update Shourov.js

--- Shourov.js
+++ Shourov.js
@@ -223,11 +223,11 @@ if (config.autoRestart) {
 // ———————————————— DASHBOARD ROUTES ———————————————— //
 
 app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/Shourov.html'));
+        res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/appstate', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/Shourovstate.html'));
+        res.sendFile(path.join(__dirname, 'public/appstate.html'));
 });
 
 app.get("/api/stats", (req, res) => {
