dbd0db51af817eef9da68636484cc831f3198cc8
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-03-04 07:43:37 -0800

    Update log.js

--- logger/log.js
+++ logger/log.js
@@ -1,19 +1,29 @@
 const { colors } = require('../func/colors.js');
 const moment = require("moment-timezone");
 const characters = '';
-const getCurrentTime = () => colors.gray(moment().tz("Asia/Ho_Chi_Minh").format("HH:mm:ss DD/MM/YYYY"));
+const getCurrentTime = () => colors.gray(moment().tz("Asia/Dhaka").format("HH:mm:ss DD/MM/YYYY"));
 
 function logError(prefix, message) {
 	if (message === undefined) {
 		message = prefix;
 		prefix = "ERROR";
 	}
-	console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, message);
+	const logMessage = `${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)} ${message}`;
+	console.log(logMessage);
+
+	// Send to dashboard if available
+	if (global.dashboardLogStream) {
+		global.dashboardLogStream(`[ERROR] ${prefix}: ${message}`);
+	}
+
 	const error = Object.values(arguments).slice(2);
 	for (let err of error) {
 		if (typeof err == "object" && !err.stack)
 			err = JSON.stringify(err, null, 2);
 		console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, err);
+		if (global.dashboardLogStream) {
+			global.dashboardLogStream(`[ERROR] ${err}`);
+		}
 	}
 }
 
@@ -26,6 +36,9 @@ module.exports = {
 			prefix = "WARN";
 		}
 		console.log(`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)}`, message);
+		if (global.dashboardLogStream) {
+			global.dashboardLogStream(`[WARN] ${prefix}: ${message}`);
+		}
 	},
 	info: function (prefix, message) {
 		if (message === undefined) {
@@ -33,6 +46,9 @@ module.exports = {
 			prefix = "INFO";
 		}
 		console.log(`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)}`, message);
+		if (global.dashboardLogStream) {
+			global.dashboardLogStream(`[INFO] ${prefix}: ${message}`);
+		}
 	},
 	success: function (prefix, message) {
 		if (message === undefined) {
@@ -40,6 +56,9 @@ module.exports = {
 			prefix = "SUCCES";
 		}
 		console.log(`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)}`, message);
+		if (global.dashboardLogStream) {
+			global.dashboardLogStream(`[SUCCESS] ${prefix}: ${message}`);
+		}
 	},
 	master: function (prefix, message) {
 		if (message === undefined) {
@@ -47,10 +66,11 @@ module.exports = {
 			prefix = "MASTER";
 		}
 		console.log(`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)}`, message);
+		if (global.dashboardLogStream) {
+			global.dashboardLogStream(`[MASTER] ${prefix}: ${message}`);
+		}
 	},
 	dev: (...args) => {
-		if (["development", "production"].includes(process.env.NODE_ENV) == false)
-			return;
 		try {
 			throw new Error();
 		}
@@ -59,6 +79,9 @@ module.exports = {
 			let position = at.slice(at.indexOf(process.cwd()) + process.cwd().length + 1);
 			position.endsWith(')') ? position = position.slice(0, -1) : null;
 			console.log(`\x1b[36m${position} =>\x1b[0m`, ...args);
+			if (global.dashboardLogStream) {
+				global.dashboardLogStream(`[DEV] ${position} => ${args.join(' ')}`);
+			}
 		}
 	}
 };
