b5d846cbcba47d5a82088d5ec7f46787fe5c079b
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-03-04 08:40:46 -0800

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,11 @@
-const axios = require('axios');
 
-axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/updater.js")
-	.then(res => eval(res.data));
+const { execSync } = require('child_process');
+const log = require('./logger/log.js');
+
+try {
+    log.info("UPDATE", "Starting update process...");
+    execSync("node updater.js", { stdio: 'inherit' });
+} catch (error) {
+    log.error("UPDATE", "Update failed:", error.message);
+    process.exit(1);
+}
