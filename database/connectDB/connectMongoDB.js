ef6ae5b2d430ece99205f17c93e526c3f6154ec5
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-03-04 08:15:37 -0800

    Update connectMongoDB.js

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -1,17 +1,25 @@
 module.exports = async function (uriConnect) {
-				const mongoose = require("mongoose");
+	const mongoose = require("mongoose");
 
-				const threadModel = require("../models/mongodb/thread.js");
-				const userModel = require("../models/mongodb/user.js");
-				const dashBoardModel = require("../models/mongodb/userDashBoard.js");
-				const globalModel = require("../models/mongodb/global.js");
+	// Fix deprecation warning
+	mongoose.set('strictQuery', false);
 
-				await mongoose.connect(uriConnect);
+	const threadModel = require("../models/mongodb/thread.js");
+	const userModel = require("../models/mongodb/user.js");
+	const dashBoardModel = require("../models/mongodb/userDashBoard.js");
+	const globalModel = require("../models/mongodb/global.js");
+	const bankModel = require("../models/mongodb/bank.js");
 
-				return {
-								threadModel,
-								userModel,
-								dashBoardModel,
-								globalModel
-				};
+	await mongoose.connect(uriConnect, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+
+	return {
+		threadModel,
+		userModel,
+		dashBoardModel,
+		globalModel,
+		bankModel
+	};
 };
