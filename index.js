02043120a11b864e40ac165d93818ef8b0f14cf7
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-03-04 07:34:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-const express = require("express");
-const path = require("path");
 const { spawn } = require("child_process");
+const log = require("./logger/log.js");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+function startProject() {
+	const child = spawn("node", ["Shourov.js"], {
+		cwd: __dirname,
+		stdio: "inherit",
+		shell: true
+	});
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Web server running on port ${PORT}`);
-});
-
-function startBot(accountFileName) {
-  console.log(`[index.js] Starting bot with account file: ${accountFileName}`);
-  const env = { ...process.env, ACCOUNT_FILE: accountFileName };
-  const child = spawn("node", ["Shourov.js"], {
-    cwd: __dirname,
-    shell: true,
-    env: { ...env, NODE_OPTIONS: "--trace-warnings" }
-  });
-
-  child.stdout.on("data", (data) => {
-    process.stdout.write(data);
-  });
-
-  child.stderr.on("data", (data) => {
-    process.stderr.write(data);
-  });
-
-  child.on("close", (code) => {
-    if (code === 2) {
-      console.log(`[${accountFileName}] Bot exited with code 2. Restarting...`);
-      startBot(accountFileName);
-    } else {
-      console.log(`[${accountFileName}] Bot exited with code ${code}`);
-    }
-  });
+	child.on("close", (code) => {
+		if (code == 2) {
+			log.info("Restarting Project...");
+			startProject();
+		}
+	});
 }
 
+startProject();
 startBot("Shourov.dev.txt");
