053210faa94fa23da88506a58752a619e4c22fbc
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-03-05 14:40:52 -0800

    Update marry.js

--- scripts/cmds/marry.js
+++ scripts/cmds/marry.js
@@ -1,50 +1,77 @@
-const axios = require('axios');
+const axios = require("axios");
 const jimp = require("jimp");
-const fs = require("fs")
-
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
-    config: {
-        name: "marry",
-        aliases: ["marry"],
-        version: "1.0",
-        author: "AceGun",
-        countDown: 5,
-        role: 0,
-        shortDescription: "get a wife",
-        longDescription: "",
-        category: "marry",
-        guide: "{pn}"
-    },
+  config: {
+    name: "marry",
+    version: "3.0.0",
+    author: "Alihsan Shourov",
+    countDown: 5,
+    role: 0,
+    category: "fun",
+    guide: "{p}marry @mention OR reply OR UID"
+  },
 
+  onStart: async function ({ message, event, args }) {
+    try {
+      const one = event.senderID;
 
+      // ===== TARGET SYSTEM =====
+      let two =
+        event.messageReply?.senderID ||                // Reply
+        Object.keys(event.mentions || {})[0] ||        // Mention
+        args[0];                                       // UID
 
-    onStart: async function ({ message, event, args }) {
-        const mention = Object.keys(event.mentions);
-      if(mention.length == 0) return message.reply("Please mention someone");
-else if(mention.length == 1){
-const one = event.senderID, two = mention[0];
-                bal(one, two).then(ptth => { message.reply({ body: "「 i love you babe🥰❤️ 」", attachment: fs.createReadStream(ptth) }) })
-} else{
- const one = mention[1], two = mention[0];
-            bal(one, two).then(ptth => { message.reply({ body: "「 i love you babe🥰❤️ 」" , attachment: fs.createReadStream(ptth) }) })
-}
+      if (!two)
+        return message.reply("💍 Mention / Reply / UID দাও যাকে marry করবে!");
+
+      const imagePath = await createMarriageImage(one, two);
+
+      message.reply(
+        {
+          body: `💖 Married Successfully 💍\n\nUID: ${two}`,
+          attachment: fs.createReadStream(imagePath)
+        },
+        () => fs.unlinkSync(imagePath)
+      );
+
+    } catch (err) {
+      console.log("MARRY ERROR:", err);
+      message.reply("⚠️ Marry command error.");
     }
-
-
+  }
 };
 
-async function bal(one, two) {
 
-    let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avone.circle()
-    let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avtwo.circle()
-    let pth = "abcd.png"
-    let img = await jimp.read("https://i.imgur.com/qyn1vO1.jpg")
+// ===== IMAGE FUNCTION =====
+async function createMarriageImage(one, two) {
 
-    img.resize(432, 280).composite(avone.resize(60, 60), 189, 15).composite(avtwo.resize(60, 60), 122, 25);
+  const tmpDir = path.join(__dirname, "tmp");
+  if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir);
 
-    await img.writeAsync(pth)
-    return pth
-      }
+  const outputPath = path.join(tmpDir, `marry_${Date.now()}.png`);
+
+  // ===== Get Avatar From Graph =====
+  const avone = await jimp.read(
+    `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+  );
+
+  const avtwo = await jimp.read(
+    `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+  );
+
+  avone.circle();
+  avtwo.circle();
+
+  const background = await jimp.read("https://i.imgur.com/qyn1vO1.jpg");
+
+  background.resize(432, 280)
+    .composite(avone.resize(80, 80), 180, 10)
+    .composite(avtwo.resize(80, 80), 120, 25);
+
+  await background.writeAsync(outputPath);
+
+  return outputPath;
+}
